ff8955db676e3b2049a55bfa6853f969ad66f491
ShirinNurullahova <90415981+ShirinNurullahova@users.noreply.github.com>
2022-06-13 18:14:36 +0400

    anyway

--- src/components/YaratCenter/YaratCenter.js
+++ src/components/YaratCenter/YaratCenter.js
@@ -6,14 +6,14 @@ import '../../components/YaratCenter/YaratCenter.css'
 // import {useWindowScroll } from "react-use"
 
 const YaratCenter = ({ menuHandler }) => {
- 
+
     const slideref = useRef();
 
     useEffect(() => {
         menuHandler(true)
         // console.log('menu true')
-        
-       
+
+
 
         return () => {
             // console.log('menu false')
@@ -24,64 +24,64 @@ const YaratCenter = ({ menuHandler }) => {
         , [menuHandler])
 
 
-    const slides =[{image: imageExhibition },{image:imageLeft},{image:imageLeft },{image:imageLeft },{image:imageLeft}]
+    const slides = [{ image: imageExhibition }, { image: imageLeft }, { image: imageLeft }, { image: imageLeft }, { image: imageLeft }]
     const slideLeft = () => {
-    //    console.log("sssss")
-         slideref.current.scrollLeft -= 200
-        
-      };
+        //    console.log("sssss")
+        slideref.current.scrollLeft -= 200
 
-      const slideRight = () => {
-        slideref.current.scrollTo({behavior: "smooth"})
-        slideref.current.scrollLeft += 200 
-    
-      };    
+    };
+
+    const slideRight = () => {
+        slideref.current.scrollTo({ behavior: "smooth" })
+        slideref.current.scrollLeft += 200
+
+    };
     return (
         <div className="exhibition">
 
             <div className="middleBox">
-                <p  className="middleBoxP"> EXHIBITIONS</p>
+                <p className="middleBoxP"> EXHIBITIONS</p>
                 <h1>YARAT CENTRE</h1>
-               
+
                 <div className="imageBox" id="imageBox" ref={slideref}>
 
-                      <div className="position">
+                    <div className="position">
                         <div className="positionUp">
                             <img src={redEllipse}></img>
                             <p>NOW ON SHOW</p>
                         </div>
                         <div className="positionDown">
                             <p className="textDown"><span className="border">YARAT CENTRE: </span>
-                              <span>
-                                   LAST FLOWERS OF  CENTURY – GENERATION 
+                                <span>
+                                    LAST FLOWERS OF  CENTURY – GENERATION
                                     Y GROUP EXHIBITION /
-                              </span>
+                                </span>
                                 <span className="dif">10 JUN - 30 DEC 21</span>
-                                </p>
+                            </p>
                         </div>
                     </div>
-                              <div  id="slider">
-                                     {
-                                         slides.map((slide,index )=>{
-                                             return (<div className="image" key={index}>
-                                                  <img src={slide.image} />
-                                             </div>)
-                                             
-                                         })
-                                     }
-                              </div>
-                   
+                    <div id="slider">
+                        {
+                            slides.map((slide, index) => {
+                                return (<div className="image" key={index}>
+                                    <img src={slide.image} />
+                                </div>)
+
+                            })
+                        }
+                    </div>
+
                 </div>
                 <div className="textPosition">
-                    <button  className="left" onClick={slideLeft}>LEFT</button>
+                    <button className="left" onClick={slideLeft}>LEFT</button>
                     <button onClick={slideRight} className="right">RIGHT</button>
                 </div>
             </div>
 
-            
+
 
         </div>
-        
+
     )
 }
 export default YaratCenter
